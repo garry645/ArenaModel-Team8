3f1827299c00d89094bd9cf169717035f665725e
Garry <garrisonhatcher645@gmail.com>
2021-02-22 21:33:58 -0600

    Summary
    First Commit

---
+++ Assets/Scripts/ColorChangingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangingScript : MonoBehaviour
+{
+
+    Light lt;
+    //bool colorChanged = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+        StartCoroutine(updateColor());
+        //lt.color = Color.blue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator updateColor() {
+    
+        while(true) {
+
+            lt.color = Color.blue;
+            yield return new WaitForSeconds(2);
+            lt.color = new Color(.8666667f, 0.3333333f, 0.04705882f);
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
---
+++ Assets/Scripts/RowScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> seats = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
