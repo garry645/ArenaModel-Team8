8a074693153aa2bd010941cbf970f6075eb6bcb3
Garry <garrisonhatcher645@gmail.com>
2021-04-25 21:13:23 -0500

    Lights connected, end of Cycle 3
    Lights connected, end of Cycle 3

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class CameraControls : MonoBehaviour
+{
+    
+    public float movementSpeed = 10f;
+
+    public float fastMovementSpeed = 100f;
+
+    public float freeLookSensitivity = 3f;
+
+    public float zoomSensitivity = 10f;
+
+    public float fastZoomSensitivity = 50f;
+
+    private float x;
+    private float y;
+    private Vector3 rotateValue;
+
+    private bool looking = false;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        var fastMode = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        var movementSpeed = fastMode ? this.fastMovementSpeed : this.movementSpeed;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + (-transform.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + (transform.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + (transform.forward * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + (-transform.forward * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position = transform.position + (transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.position = transform.position + (-transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.PageUp))
+        {
+            transform.position = transform.position + (Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.PageDown))
+        {
+            transform.position = transform.position + (-Vector3.up * movementSpeed * Time.deltaTime);
+        }
+        
+        y = Input.GetAxis("Mouse X") * 5f;
+        x = Input.GetAxis("Mouse Y") * 5f;
+        Debug.Log(x + ":" + y);
+        rotateValue = new Vector3(x, y * -1, 0);
+        transform.eulerAngles = transform.eulerAngles - rotateValue;
+
+        if (looking)
+        {
+            float newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * freeLookSensitivity;
+            float newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * freeLookSensitivity;
+            transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
+        }
+
+        float axis = Input.GetAxis("Mouse ScrollWheel");
+        if (axis != 0)
+        {
+            var zoomSensitivity = fastMode ? this.fastZoomSensitivity : this.zoomSensitivity;
+            transform.position = transform.position + transform.forward * axis * zoomSensitivity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartLooking();
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            StopLooking();
+        }
+    }
+
+    void OnDisable()
+    {
+        StopLooking();
+    }
+
+    /// <summary>
+    /// Enable free looking.
+    /// </summary>
+    public void StartLooking()
+    {
+        looking = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    /// <summary>
+    /// Disable free looking.
+    /// </summary>
+    public void StopLooking()
+    {
+        looking = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
--- Assets/Scripts/ColorChangingScript.cs
+++ Assets/Scripts/ColorChangingScript.cs
@@ -6,29 +6,85 @@ public class ColorChangingScript : MonoBehaviour
 {
 
     Light lt;
-    //bool colorChanged = false;
-    // Start is called before the first frame update
+    public Light leftLight;
+    public Light rightLight;
+    public List<Color> colors;
+    
     void Start()
     {
         lt = GetComponent<Light>();
-        StartCoroutine(updateColor());
+        //StartCoroutine(updateColor());
         //lt.color = Color.blue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Alpha1)) {
+            if(colors.Count > 0 && colors[0] != null) {
+                lt.color = colors[0];
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Alpha2)) {
+            if(colors.Count > 1 && colors[1] != null) {
+                lt.color = colors[1];
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Alpha3)) {
+            if(colors.Count > 2 && colors[2] != null) {
+                lt.color = colors[2];
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Alpha4)) {
+            if(colors.Count > 3 && colors[3] != null) {
+                lt.color = colors[3];
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Alpha5)) {
+            if(colors.Count > 3 && colors[3] != null) {
+                lt.color = colors[4];
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Alpha6)) {
+            if(colors.Count > 3 && colors[3] != null) {
+                lt.color = colors[5];
+            }
+        }
+
+        if(Input.GetKey(KeyCode.J)) {
+            if (leftLight != null) {
+                 StartCoroutine(updateLight(leftLight, this.lt.color));
+            }
+        }
+
+        if(Input.GetKey(KeyCode.K)) {
+            if(rightLight != null) {
+                StartCoroutine(updateLight(rightLight, this.lt.color));
+            } else {
+                //light.color = new Color(207, 131, 28);
+            }
+        }
+
+        if(Input.GetKey(KeyCode.B)){
+            lt.color = Color.red;
+        }
+
+        if(Input.GetKey(KeyCode.N)){
+            lt.color = Color.green;
+        }
+
+        if(Input.GetKey(KeyCode.M)){
+            lt.color = Color.blue;
+        }
     }
 
-    IEnumerator updateColor() {
-    
-        while(true) {
-
-            lt.color = Color.blue;
-            yield return new WaitForSeconds(2);
-            lt.color = new Color(.8666667f, 0.3333333f, 0.04705882f);
-            yield return new WaitForSeconds(2);
-        }
+    IEnumerator updateLight(Light light, Color color) {
+        yield return new WaitForSeconds(.1f);
+        light.color = color;
     }
 }
